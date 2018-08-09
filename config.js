661beadde424d68870f12908a17b950b253f0483
Adrian Meller <melleradria@gmail.com>
2018-08-09 23:04:54 +0200

    Add basic files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '101678321420-r3jqh0ofohm2ii7r4nopdsb1fkil1tqt.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '8RYkc4Um6fJtSyXIs4ORkLQ1',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
